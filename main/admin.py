7d584c1126e6441e7bba16cf4b7b3b7e6bc18fe7
Anatoly <code4webdev@gmail.com>
2026-07-31 15:10:44 +0500

    Регистрация модели 'Furniture' в админ-панели с настройками отображения и поиска

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Furniture
 
-# Register your models here.
+@admin.register(Furniture)
+class FurnitureAdmin(admin.ModelAdmin):
+  list_display=("id", "name", "price")
+  search_fields = ("name",)
