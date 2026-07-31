b4ba3c229f74ab2185d1df55bdcb973dfc776876
Anatoly <code4webdev@gmail.com>
2026-07-31 14:58:18 +0500

    Создание модели 'Furniture' и миграции для базы данных

--- main/models.py
+++ main/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class Furniture(models.Model):
+  name = models.CharField('Название', max_length=30)
+  price = models.DecimalField(max_digits=10, decimal_places=2)
