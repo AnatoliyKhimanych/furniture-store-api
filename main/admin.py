1a631c113afcf3b4991b9466f24ebedd82f2c982
Anatoly <code4webdev@gmail.com>
2026-08-07 14:17:02 +0500

    Добавление поля 'category' в отображение модели 'Furniture' в админ-панели

--- main/admin.py
+++ main/admin.py
@@ -3,5 +3,5 @@ from .models import Furniture
 
 @admin.register(Furniture)
 class FurnitureAdmin(admin.ModelAdmin):
-  list_display=("id", "name", "price")
+  list_display=("id", "name", "price", "category")
   search_fields = ("name",)
