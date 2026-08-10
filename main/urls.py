4275cbe7b3c387efdd5325de8f0422f8d491aa8c
Anatoly <code4webdev@gmail.com>
2026-08-11 01:20:42 +0500

    Добавление маршрута для получения деталей мебели по ID

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('furniture/', views.get_queryset)
+    path('furniture/', views.get_queryset),
+    path('furniture/<int:pk>/', views.get_furniture_detail)
 ]
--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from . import models
@@ -17,5 +17,13 @@ def get_queryset(request):
     furniture = furniture.filter(category=category)
 
   serializer = serializers.FurnitureSerializer(furniture, many=True)
+
+  return Response(serializer.data)
+
+@api_view(['GET'])
+def get_furniture_detail(request, pk):
+  furniture = get_object_or_404(models.Furniture, pk=pk)
+
+  serializer = serializers.FurnitureSerializer(furniture)
 
   return Response(serializer.data)
