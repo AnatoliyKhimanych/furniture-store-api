655718ffb717512fe34949d4e720b7878d0a9ae0
Anatoly <code4webdev@gmail.com>
2026-08-14 00:31:49 +0500

    Добавление маршрута для обработки заказов в файле urls.py

--- main/urls.py
+++ main/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('furniture/', views.get_queryset),
-    path('furniture/<int:pk>/', views.get_furniture_detail)
+    path('furniture/<int:pk>/', views.get_furniture_detail),
+    path('orders/', views.orders)
 ]
