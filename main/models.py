8d4a3a39df30f366f4fd888a86caac0e76f0185e
Anatoly <code4webdev@gmail.com>
2026-08-14 00:27:57 +0500

    Добавление модели Order для обработки заказов

--- main/models.py
+++ main/models.py
@@ -10,3 +10,9 @@ class Furniture(models.Model):
   name = models.CharField('Название', max_length=30)
   price = models.DecimalField(max_digits=10, decimal_places=2)
   category = models.CharField(max_length=10, choices=CATEGORIES, db_index=True)
+
+class Order(models.Model):
+  email = models.EmailField()
+  goods_list = models.ManyToManyField(Furniture)
+  amount = models.DecimalField(max_digits=10, decimal_places=2)
+  date = models.DateTimeField()
