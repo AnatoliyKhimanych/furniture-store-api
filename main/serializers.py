78e8b930cce674cc1533355a3438af4bbd4dbc9b
Anatoly <code4webdev@gmail.com>
2026-08-14 10:08:33 +0500

    Добавление сериализатора для модели Order

--- main/serializers.py
+++ main/serializers.py
@@ -1,9 +1,16 @@
 from rest_framework import serializers
-from .models import Furniture
+from .models import Furniture, Order
 
 class FurnitureSerializer(serializers.ModelSerializer):
 
   class Meta:
     model = Furniture
+
+    fields = '__all__'
+
+class OrderSerializer(serializers.ModelSerializer):
+
+  class Meta:
+    model = Order
 
     fields = '__all__'
